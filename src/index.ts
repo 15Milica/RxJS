ad56ca9797ae770ce4b897dba6a5b5e5d4c91de9
15Milica <stmilica007@gmail.com>
2023-06-16 20:53:12 +0200

    Pocetak

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+console.log("milica");
+
+const divPoc = document.createElement("div");
+document.body.appendChild(divPoc);
+let labela = document.createElement("h1");
+labela.innerHTML="Competition";
+divPoc.appendChild(labela);
+const input = document.createElement("input");
+divPoc.appendChild(input);
